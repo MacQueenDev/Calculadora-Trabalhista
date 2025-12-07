5de84b8237087f82dc3b91406c1f352d4d8bf5ee
marcos <marcos.eredin123@gmail.com>
2025-12-07 17:52:54 -0300

    inicio e teste do spring

---
+++ Backend/calculadora-trabalhista-backend/src/main/java/com/github/macqueendev/calculadora_trabalhista_backend/CalculadoraTrabalhistaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.macqueendev.calculadora_trabalhista_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraTrabalhistaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraTrabalhistaBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/calculadora-trabalhista-backend/src/test/java/com/github/macqueendev/calculadora_trabalhista_backend/CalculadoraTrabalhistaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.macqueendev.calculadora_trabalhista_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraTrabalhistaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
